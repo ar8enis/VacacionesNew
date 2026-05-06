0b91c57144a78396fd605ee86a784144c4c72bcf
ar8enis <60239186+ar8enis@users.noreply.github.com>
2026-05-06 13:37:46 -0600

    Delete models directory

--- models/VacationRule.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
-
-const VacationRule = sequelize.define('VacationRule', {
-  anios: { type: DataTypes.INTEGER, allowNull: false, unique: true },
-  diasDerecho: { type: DataTypes.INTEGER, allowNull: false }
-});
-
-module.exports = VacationRule;
